57ae6c1841a989c086421dfea5d2b544f746bb83
Gkmaik <164330643+Gkmaik@users.noreply.github.com>
2024-04-11 15:55:19 +0200

    Update views

---
+++ Módulo 2 - SQL Server/Avanzado (opcional)/vArtistasConAlbum.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[vArtistasConAlbum]
+	AS SELECT Ar.[Name], COUNT(Al.AlbumId) AS N_Albums
+			FROM dbo.Artist Ar
+			LEFT JOIN dbo.Album Al
+			ON Ar.ArtistId = Al.ArtistId
+			GROUP BY Ar.[Name];
---
+++ Módulo 2 - SQL Server/Avanzado (opcional)/vListaDeClientesYsuPaís.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[vListaDeClientesYsuPaís]
+	AS SELECT C.CustomerId
+			  ,C.FirstName
+			  ,C.LastName
+			  ,C.Country
+		  FROM dbo.Customer C
+		  WHERE C.Country IN ('USA','Canada','Brazil')
